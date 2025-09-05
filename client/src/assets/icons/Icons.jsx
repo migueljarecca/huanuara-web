ec3efcac60fcc674788486ad0d93244fee53373e
miguel <miguel.jarecca@gmail.com>
2025-09-05 17:43:05 -0500

    created html for transparency section

--- client/src/assets/icons/Icons.jsx
+++ client/src/assets/icons/Icons.jsx
@@ -67,3 +67,24 @@ export const BookOpen = () => {
         </svg>
     )
 }
+
+/* ============================================================ */
+/* Home transparency */
+/* ============================================================ */
+
+/* hi2 HiOutlineFolder */
+export const Folder = () => {
+    return (
+        <svg stroke="currentColor" fill="none" stroke-width="1.5" viewBox="0 0 24 24" aria-hidden="true" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"></path>
+        </svg>
+    )    
+}
+
+export const ArrowSmallRight = () => {
+    return (
+        <svg stroke="currentColor" fill="none" stroke-width="1.5" viewBox="0 0 24 24" aria-hidden="true" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0-6.75-6.75M19.5 12l-6.75 6.75"></path>
+        </svg>
+    )
+}
--- client/src/pages/public/Home.jsx
+++ client/src/pages/public/Home.jsx
@@ -1,5 +1,5 @@
-import { BookOpen, ChevronRight } from "../../assets/icons/Icons"
-import { NavLink } from "react-router-dom"
+import { ArrowSmallRight, BookOpen, ChevronRight, Folder } from "../../assets/icons/Icons"
+import { Link, NavLink } from "react-router-dom"
 
 import homeServicesCard1 from "../../assets/images/home/ServicesCard/home-services-card-1.png"
 import homeServicesCard2 from "../../assets/images/home/ServicesCard/home-services-card-2.webp"
@@ -96,66 +96,189 @@ export const Home = () => {
 
                     <div className="home-services__grid">
 
-                        {/* <div className="home-services__wrapper-card"> */}
-                            <div className="home-services__card">
-                                <NavLink to={'/login'} className="card-link"></NavLink> 
-                                <div className="card-wrapper-img">
-                                    <img src={homeServicesCard1} alt="Service Icon 1" />
-                                </div>
-
-                                <div className="card-body">
-                                    <h3>SISFOH</h3>
-                                    <p>Focalización de hogares para programas sociales</p>
-                                </div>
-
-                                <div className="card-action">
-                                    <span>Ver más detalles</span>
-                                    <ChevronRight />
-                                </div>
+                        <div className="home-services__card">
+                            <NavLink to={'/login'} className="card-link"></NavLink> 
+                            <div className="card-wrapper-img">
+                                <img src={homeServicesCard1} alt="Service Icon 1" />
                             </div>
-                        {/* </div> */}
 
-                        <div className="home-services__wrapper-card">
-                            <div className="home-services__card">
-                                <NavLink to={'/login'} className="card-link"></NavLink> 
-                                <div className="card-wrapper-img">
-                                    <img src={homeServicesCard2} alt="Service Icon 1" />
-                                </div>
+                            <div className="card-body">
+                                <h3 className="card-title">SISFOH</h3>
+                                <p className="card-subtitle">Focalización de hogares para programas sociales</p>
+                            </div>
 
-                                <div className="card-body">
-                                    <h3>SISFOH</h3>
-                                    <p>Focalización de hogares para programas sociales</p>
-                                </div>
-
-                                <div className="card-action">
-                                    <span>Ver más detalles</span>
+                            <div className="card-action">
+                                <span>Visitar página</span>
+                                <div className="wrapper-icon">
                                     <ChevronRight />
                                 </div>
                             </div>
                         </div>
 
-                        <div className="home-services__wrapper-card">
-                            <div className="home-services__card">
-                                <NavLink to={'/login'} className="card-link"></NavLink> 
-                                <div className="card-wrapper-img">
-                                    <img src={homeServicesCard3} alt="Service Icon 1" />
-                                </div>
+                        <div className="home-services__card">
+                            <NavLink to={'/login'} className="card-link"></NavLink> 
+                            <div className="card-wrapper-img">
+                                <img src={homeServicesCard2} alt="Service Icon 1" />
+                            </div>
 
-                                <div className="card-body">
-                                    <h3>SISFOH</h3>
-                                    <p>Focalización de hogares para programas sociales</p>
-                                </div>
+                            <div className="card-body">
+                                <h3 className="card-title">SISFOH</h3>
+                                <p className="card-subtitle">Focalización de hogares para programas sociales</p>
+                            </div>
 
-                                <div className="card-action">
-                                    <span>Ver más detalles</span>
+                            <div className="card-action">
+                                <span>Visitar página</span>
+                                <div className="wrapper-icon">
+                                    <ChevronRight />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="home-services__card">
+                            <NavLink to={'/login'} className="card-link"></NavLink> 
+                            <div className="card-wrapper-img">
+                                <img src={homeServicesCard3} alt="Service Icon 1" />
+                            </div>
+
+                            <div className="card-body">
+                                <h3 className="card-title">SISFOH</h3>
+                                <p className="card-subtitle">Focalización de hogares para programas sociales</p>
+                            </div>
+
+                            <div className="card-action">
+                                <span>Visitar página</span>
+                                <div className="wrapper-icon">
                                     <ChevronRight />
                                 </div>
                             </div>
                         </div>                        
+                     
+                        <div className="home-services__card">
+                            <NavLink to={'/login'} className="card-link"></NavLink> 
+                            <div className="card-wrapper-img">
+                                <img src={homeServicesCard2} alt="Service Icon 1" />
+                            </div>
 
+                            <div className="card-body">
+                                <h3 className="card-title">SISFOH</h3>
+                                <p className="card-subtitle">Focalización de hogares para programas sociales</p>
+                            </div>
+
+                            <div className="card-action">
+                                <span>Visitar página</span>
+                                <div className="wrapper-icon">
+                                    <ChevronRight />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="home-services__card">
+                            <NavLink to={'/login'} className="card-link"></NavLink> 
+                            <div className="card-wrapper-img">
+                                <img src={homeServicesCard3} alt="Service Icon 1" />
+                            </div>
+
+                            <div className="card-body">
+                                <h3 className="card-title">SISFOH</h3>
+                                <p className="card-subtitle">Focalización de hogares para programas sociales</p>
+                            </div>
+
+                            <div className="card-action">
+                                <span>Visitar página</span>
+                                <div className="wrapper-icon">
+                                    <ChevronRight />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="home-services__see-more">
+                        <Link to={'/login'} className="home-services__see-more-link">
+                            Ver todos los servicios
+                        </Link>
                     </div>
 
                 </div>        
+            </section>
+
+            <section className="home-transparency">
+
+                <div className="home-transparency__container">
+                    <div className="home-transparency__content">
+                        <h2 className="home-transparency__title">Documentos de Transparencia</h2>
+                        <p className="subtitle">
+                            En esta sección encontrarás información pública sobre la gestión municipal. 
+                            Nuestro objetivo es garantizar la transparencia y el acceso libre a los documentos que 
+                            fortalecen la confianza entre la municipalidad y la comunidad."</p>
+                    </div>
+                </div>
+
+                <div className="home-transparency__grid">
+
+                    <div className="home-transparency__card">
+                        <div className="wrapper-icon">
+                            <Folder />
+                        </div>
+
+                        <div className="card-body">
+                            <h3 className="card-title">Presupuesto Municipal</h3>
+                            <p className="card-subtitle">Consulta los presupuestos anuales asignados a la municipalidad y cómo se distribuyen en cada área.</p>
+                        </div>
+
+
+                        <div className="card-action">
+                            <Link to={'/login'} className="home-transparency__see-more-link">
+                                <span>Ver más detalles</span>
+                                <div className="wrapper-icon">
+                                    <ArrowSmallRight />
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+
+                    <div className="home-transparency__card">
+                        <div className="wrapper-icon">
+                            <Folder />
+                        </div>
+
+                        <div className="card-body">
+                            <h3 className="card-title">Obras y Proyectos</h3>
+                            <p className="card-subtitle">Información sobre las obras en ejecución y proyectos planificados para la comunidad.</p>
+                        </div>
+
+
+                        <div className="card-action">
+                            <Link to={'/login'} className="home-transparency__see-more-link">
+                                <span>Ver más detalles</span>
+                                <div className="wrapper-icon">
+                                    <ArrowSmallRight />
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+
+                    <div className="home-transparency__card">
+                        <div className="wrapper-icon">
+                            <Folder />
+                        </div>
+
+                        <div className="card-body">
+                            <h3 className="card-title">Rendición de Cuentas</h3>
+                            <p className="card-subtitle">Revisa los informes de gastos e ingresos, garantizando la transparencia en la gestión.</p>
+                        </div>
+
+
+                        <div className="card-action">
+                            <Link to={'/login'} className="home-transparency__see-more-link">
+                                <span>Ver más detalles</span>
+                                <div className="wrapper-icon">
+                                    <ArrowSmallRight />
+                                </div>
+                            </Link>
+                        </div>
+                    </div>                                        
+
+                </div>
             </section>
         </main>
     )
